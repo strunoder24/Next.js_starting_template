80e507cf95273c3fe8909f30f658a49774ca8340
Виктор Кондик <strunoder24@mail.ru>
2019-08-31 12:20:00 +1000

    Проверил реактивность стандартного кода реакта. Всё норм

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-const Index = () => (
-    <div>
-        Hello World, it's me using next.js
-    </div>
-);
+import React from 'react';
+import propTypes from 'prop-types';
 
-export default Index;
+export default class extends React.Component {
+
+    static propTypes = {};
+
+    state = {
+        number: 1,
+    };
+
+    render() {
+        return (
+            <>
+                <h1>
+                    hey boah
+                </h1>
+                <div style={{display: 'flex'}}>
+                    <button
+                        onClick={() => {
+                            this.setState({number: this.state.number - 1})
+                        }}
+                    >-</button>
+                    <span>{this.state.number}</span>
+                    <button
+                        onClick={() => {
+                            this.setState({number: this.state.number + 1})
+                        }}
+                    >+</button>
+                </div>
+            </>
+        )
+    }
+}
